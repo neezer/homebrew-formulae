bc1864624d6b36bb545c25ce65542edc308dbed1
Evan Sherwood <evan@sherwood.io>
2017-07-07 12:20:40 -0700

    gawk is not a system dependency

--- Formula/tinge.rb
+++ Formula/tinge.rb
@@ -3,7 +3,7 @@ class Tinge < Formula
   homepage "https://github.com/neezer/tinge"
   url "https://github.com/neezer/tinge/archive/v0.0.1.tar.gz"
   sha256 "4411b19e04947209a2d4205eff2f1615754b5319"
-  depends_on :gawk => :run
+  depends_on "gawk" => :run
 
   bin.install "tinge"
 
