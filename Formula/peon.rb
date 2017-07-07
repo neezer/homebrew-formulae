f70df35fb3a7fca34a69ca451779f9552827dff9
Evan Sherwood <evan@sherwood.io>
2017-07-07 12:56:35 -0700

    Add peon sha

--- Formula/peon.rb
+++ Formula/peon.rb
@@ -2,7 +2,7 @@ class Peon < Formula
   desc "Bash command runner"
   homepage "https://github.com/neezer/peon"
   url "https://github.com/neezer/peon/archive/v0.0.1.tar.gz"
-  sha256 ""
+  sha256 "0dba73cf0e1531f85c5000cd878845c95254a5fadf8fba639b8f553b1ebf3700"
 
   depends_on "gawk" => :run
   depends_on "tinge" => :run
