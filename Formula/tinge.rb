7a589f92cc40c60fafb895391367676a2119bc89
Evan Sherwood <evan@sherwood.io>
2017-07-07 12:39:26 -0700

    Update version

--- Formula/tinge.rb
+++ Formula/tinge.rb
@@ -1,7 +1,7 @@
 class Tinge < Formula
   desc "Small helper for colorizing output with ANSI escape sequences"
   homepage "https://github.com/neezer/tinge"
-  url "https://github.com/neezer/tinge/archive/v0.0.1.tar.gz"
+  url "https://github.com/neezer/tinge/archive/v0.0.2.tar.gz"
   sha256 "e3382cdec7793b8113804e74d49b529aef8f0837cf086582890c18533af109d0"
 
   def install
