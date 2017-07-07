e866f4ed4c71797d5226f0b5a5f395ea83f720fd
Evan Sherwood <evan@sherwood.io>
2017-07-07 12:27:57 -0700

    Update sha

--- Formula/tinge.rb
+++ Formula/tinge.rb
@@ -2,7 +2,7 @@ class Tinge < Formula
   desc "Small helper for colorizing output with ANSI escape sequences"
   homepage "https://github.com/neezer/tinge"
   url "https://github.com/neezer/tinge/archive/v0.0.1.tar.gz"
-  sha256 "4411b19e04947209a2d4205eff2f1615754b5319"
+  sha256 "e3382cdec7793b8113804e74d49b529aef8f0837cf086582890c18533af109d0"
   depends_on "gawk" => :run
 
   def install
