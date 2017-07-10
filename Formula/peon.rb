c1aee0991a45180b7dc13e40e49775a30e98aec6
Evan Sherwood <evan@sherwood.io>
2017-07-10 15:59:19 -0700

    Update peon version

--- Formula/peon.rb
+++ Formula/peon.rb
@@ -1,7 +1,7 @@
 class Peon < Formula
   desc "Bash command runner"
   homepage "https://github.com/neezer/peon"
-  url "https://github.com/neezer/peon/archive/v0.0.3.tar.gz"
+  url "https://github.com/neezer/peon/archive/v0.1.0.tar.gz"
   sha256 "b723c77edb35a6a06b5d87ff6570783def7fe22bb86a1fc4b1616711bccbb5a5"
 
   depends_on "gawk" => :run
