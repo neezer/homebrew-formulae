c04188864d1ab621c7c4c33cbb3f0a9090a875b5
Evan Sherwood <evan@sherwood.io>
2017-07-11 11:08:56 -0700

    Bump peon

--- Formula/peon.rb
+++ Formula/peon.rb
@@ -1,7 +1,7 @@
 class Peon < Formula
   desc "Bash command runner"
   homepage "https://github.com/neezer/peon"
-  url "https://github.com/neezer/peon/archive/v0.1.1.tar.gz"
+  url "https://github.com/neezer/peon/archive/v0.1.2.tar.gz"
   sha256 "8528a7b3d3ce45633d874e292f8ccce20300839cc97e03bddffdbb2c8fb571b4"
 
   depends_on "gawk" => :run
